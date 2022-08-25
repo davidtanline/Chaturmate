0c344fc53ce5cf5a60b87000c3b72576de2d5a27
David Tan <davidtanline@gmail.com>
2022-08-25 15:04:35 -0700

    fixed problem

--- src/Server.java
+++ src/Server.java
@@ -1,126 +1,45 @@
-package src;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class Server implements Runnable {
+public class Server {
 
-    private ArrayList<ConnectionHandler> connections;
-    private ServerSocket server;
-    private boolean done;
-    private ExecutorService pool;
+    private ServerSocket serverSocket;
 
-    public Server() {
-        connections = new ArrayList<>();
-        done = false;
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
     }
 
-    @Override
-    public void run() {
+    public void startServer() {
+
         try {
-            ServerSocket server = new ServerSocket(3000);
-            pool = Executors.newCachedThreadPool();
-            while (!done) {
-                Socket client = server.accept();
-                ConnectionHandler handler = new ConnectionHandler(client);
-                connections.add(handler);
-                pool.execute(handler);
-            }
-        } catch (Exception e) {
-            shutDown();
-        }
-    }
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected!");
+                ClientHandler clientHandler = new ClientHandler(socket);
 
-    public void broadcast(String message) {
-        for (ConnectionHandler ch : connections) {
-            if (ch != null) {
-                ch.sendMessage(message);
-            }
-        }
-    }
-
-    public void shutDown() {
-        try {
-            done = true;
-            if (!server.isClosed()) {
-                server.close();
-            }
-            for (ConnectionHandler ch : connections) {
-                ch.shutDown();
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
         } catch (IOException e) {
-            // ignore
+
+        }
+
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    class ConnectionHandler implements Runnable {
-
-        private Socket client;
-        private BufferedReader in;
-        private PrintWriter out;
-        private String nickname;
-
-        public ConnectionHandler(Socket client) {
-            this.client = client;
-        }
-
-        @Override
-        public void run() {
-            try {
-                out = new PrintWriter(client.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                out.println("Please enter your nickname:");
-                nickname = in.readLine();
-                if (!nickname.isBlank()) {
-                    System.out.println(nickname + " connected.");
-                    broadcast(nickname + " connected.");
-                }
-                String message;
-                while ((message = in.readLine()) != null) {
-                    if (message.startsWith("/nick ")) {
-                        String[] messageSplit = message.split(" ", 2);
-                        if (messageSplit.length == 2) {
-                            broadcast(nickname + " has changed their nickname to " + messageSplit[1]);
-                            nickname = messageSplit[1];
-                        }
-                    } else if (message.startsWith("/quit")) {
-                        broadcast(nickname + " left the chat.");
-                        shutDown();
-                    } else {
-                        broadcast(nickname + ": " + message);
-                    }
-                }
-            } catch (IOException e) {
-                shutDown();
-            }
-        }
-
-        public void sendMessage(String message) {
-            out.println(message);
-        }
-
-        public void shutDown() {
-            try {
-                in.close();
-                out.close();
-                if (!client.isClosed()) {
-                    client.close();
-                }
-            } catch (IOException e) {
-                // ignore
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.run();
+    public static void main (String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
